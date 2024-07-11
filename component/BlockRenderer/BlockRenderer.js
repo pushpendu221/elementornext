f2f04c74622c40022bbaa4f7f6d52c0274d22526
Pushpendu221 <pushpendu221@gmail.com>
2024-07-11 18:40:13 +0530

    updating the widget type

--- component/BlockRenderer/BlockRenderer.js
+++ component/BlockRenderer/BlockRenderer.js
@@ -1,7 +1,4 @@
 "use client";
-
-import { Column } from "component/Column";
-import { Section } from "component/Section";
 import { Widget } from "component/Widget";
 
 export const BlockRenderer = ({ blocks }) => {
@@ -13,36 +10,36 @@ export const BlockRenderer = ({ blocks }) => {
   //console.log("props", elementorData);
   return( elementorData.map((row) => {
      //console.log({ row });
-          // console.log({ column });
-               // console.log({ widget });
-                switch(row.elType){
-                  case 'section':
-                  {
-                  //  console.log("blCoks",blockstr);
-                    return(
-                      <section key={row.id} className="row">
-                          <BlockRenderer key={row.id} blocks={row.elements} />
-                      </section>
-                    )
-                  }
-                  case 'column':
-                    {
-                      return( 
-                        <div key={row.id} className={`col-${row.settings._column_size}`}>
-                            <BlockRenderer key={row.id} blocks={row.elements} />
-                        </div>
-                      )
-                    }
-                  case 'widget':
-                    {
-                      return(
-                        <Widget key={row.id} widget={row.elements} />
-                      )
-                    }  
-                  default:
-                    console.log("unknown", row);
-                    return null;
-                }
+          switch(row.elType){
+            case 'section':
+            {
+            //  console.log("blCoks",blockstr);
+              return(
+                <section key={row.id} className="row">
+                    <BlockRenderer key={row.id} blocks={row.elements} />
+                </section>
+              )
+            }
+            case 'column':
+              {
+                return( 
+                  <div key={row.id} className={`col-${row.settings._column_size}`}>
+                      <BlockRenderer key={row.id} blocks={row.elements} />
+                  </div>
+                )
+              }
+            case 'widget':
+              {
+                return(
+                  <div key={row.id} className="widget-class">
+                  <BlockRenderer key={row.id} blocks={row.elements} />
+                 </div>
+                )
+              }  
+            default:
+              console.log("unknown", row);
+              return null;
+          }
   }));
   // return {Page};
 };
